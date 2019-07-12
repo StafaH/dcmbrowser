06d0a4ec7f79da25a02d44166feff85762941d88
StafaH <mhaiderbhai@hotmail.com>
2019-07-12 22:29:53 +0200

    Update build.bat to include some additional compiler options

--- misc/build.bat
+++ misc/build.bat
@@ -20,11 +20,11 @@ pushd %BuildDir%
 :: /I look for other include directories
 :: /W# Warnings
 :: /MD Which runtime library to use (Static/Dynamic)
-set CommonCompilerFlags=/O2 /nologo /Zi /FC /EHsc /I"..\deps\include" /W4 /MD -DIMGUI_IMPL_OPENGL_LOADER_GLEW 
+set CommonCompilerFlags=/O2 /nologo -fp:fast -fp:except- -Gm- -GR- /MP /Zi /FC /EHsc /I"..\deps\include" /W4 /MD /std:c++latest -DIMGUI_IMPL_OPENGL_LOADER_GLEW 
 
 :: libs ws2_32.lib iphlpapi, ws2_32, netapi32 and wsock32 are needed for dcmtk (as well as others)
 :: The order of compilation is also important
-set CommonLinkerFlags=user32.lib gdi32.lib winmm.lib opengl32.lib kernel32.lib shell32.lib glu32.lib winspool.lib ole32.lib oleaut32.lib uuid.lib comdlg32.lib advapi32.lib netapi32.lib wsock32.lib ws2_32.Lib iphlpapi.lib
+set CommonLinkerFlags=/OPT:REF user32.lib gdi32.lib winmm.lib opengl32.lib kernel32.lib shell32.lib glu32.lib winspool.lib ole32.lib oleaut32.lib uuid.lib comdlg32.lib advapi32.lib netapi32.lib wsock32.lib ws2_32.Lib iphlpapi.lib
 set GLFWLibPath=/LIBPATH:..\deps\lib\GLFW\lib-vc2017 glfw3.lib
 set GLEWLibPath=..\deps\lib\GLEW\glew32s.lib ..\deps\lib\GLEW\glew32.lib
 set dcmtkPath=/LIBPATH:..\deps\lib\dcmtk ofstd.lib oflog.lib dcmdata.lib dcmimgle.lib 
@@ -32,8 +32,9 @@ set dcmtkPath=/LIBPATH:..\deps\lib\dcmtk ofstd.lib oflog.lib dcmdata.lib dcmimgl
 :: Code and libraries directory path
 set CodePath=..\code\*.cpp
 set ImGuiPath=..\code\imgui\*.cpp
+set tfdPath=..\code\tinyfiledialogs\tinyfiledialogs.c
 
-cl %CommonCompilerFlags% %CodePath% %ImGuiPath% /link %CommonLinkerFlags% %dcmtkPath% %GLEWLibPath% %GLFWLibpath% /out:"dcmbrowser.exe"
+cl %CommonCompilerFlags% %CodePath% %ImGuiPath% %tfdPath% /link %CommonLinkerFlags% %dcmtkPath% %GLEWLibPath% %GLFWLibpath% /out:"dcmbrowser.exe"
  
 :: Copy dependencies if needed
  
